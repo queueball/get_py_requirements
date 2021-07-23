f58821d220ce95e8aac9ef7d566c29fef898451e
queueball <queueball314159@gmail.com>
2021-07-23 01:35:04 -0700

    Updated: leverage pips ability to resolve multiple dependencies at once

--- get_py_requirements.py
+++ get_py_requirements.py
@@ -11,52 +11,51 @@ import click
 @click.argument("modules", nargs=-1)
 @click.option("--debug", is_flag=True, default=False)
 def main(modules, debug):
-    for module in modules:
-        temp_path = None
-        result = None
-        try:
-            temp_path = tempfile.mkdtemp()
-            result = subprocess.check_output(
+    temp_path = None
+    result = None
+    try:
+        temp_path = tempfile.mkdtemp()
+        result = subprocess.check_output(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "download",
+                *modules,
+                "-d",
+                temp_path,
+                "--no-binary",
+                ":all:",
+            ]
+        )
+        result = (
+            subprocess.check_output(
                 [
-                    sys.executable,
-                    "-m",
-                    "pip",
-                    "download",
-                    module,
-                    "-d",
+                    "ls",
                     temp_path,
-                    "--no-binary",
-                    ":all:",
                 ]
             )
-            result = (
-                subprocess.check_output(
-                    [
-                        "ls",
-                        temp_path,
-                    ]
-                )
-                .decode("utf-8")
-                .strip()
-            )
-            if debug:
-                print('"""')
-                print(result)
-                print('"""')
-            if result:
-                print(f"### { module }")
-            for line in result.split("\n"):
-                if line:
-                    *req, version = (
-                        line.replace(".tar.gz", "").replace(".zip", "").split("-")
-                    )
-                    print(f'{ "-".join(req) }=={ version }')
-        except Exception as e:
-            print(e)
+            .decode("utf-8")
+            .strip()
+        )
+        if debug:
+            print('"""')
             print(result)
-        finally:
-            if temp_path:
-                shutil.rmtree(temp_path, True)
+            print('"""')
+        if result:
+            print(f"### { ' '.join(sorted(modules)) }")
+        for line in sorted(result.split("\n")):
+            if line:
+                *req, version = (
+                    line.replace(".tar.gz", "").replace(".zip", "").split("-")
+                )
+                print(f'{ "-".join(req) }=={ version }')
+    except Exception as e:
+        print(e)
+        print(result)
+    finally:
+        if temp_path:
+            shutil.rmtree(temp_path, True)
 
 
 if __name__ == "__main__":
